6f075ccdd29f54251600b2d93ab3715cb7a57a59
tranvandoan1 <tranlove24@gmail.com>
2022-09-25 20:48:42 +0700

    4esaasdsa

---
+++ routes/Table.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  create,
+  list,
+  update,
+  Id,
+  read,
+  remove,
+  bookTable,
+  moveTable,
+} from "../controllers/Table";
+import { isAuthenticateUser } from "./CheckAuth";
+const router = express.Router();
+
+router.post("/table", isAuthenticateUser, create);
+router.get("/table", isAuthenticateUser, list);
+router.get("/table/:id", isAuthenticateUser, read);
+
+router.put("/table/:id", isAuthenticateUser, update);
+
+router.post("/table/book-table", isAuthenticateUser, bookTable);
+router.post("/table/move-table", isAuthenticateUser, moveTable);
+
+router.delete("/table/:id", isAuthenticateUser, remove);
+
+router.param("id", isAuthenticateUser, Id);
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  userById,
+  list,
+  remove,
+  read,
+  update,
+  listUser,
+  updateInfo,
+} from "../controllers/user";
+import { requireSignin, isAdmin, isAuth } from "../controllers/auth";
+import { isAuthenticateUser } from "./CheckAuth";
+
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+  res.json({
+    user: req.profile,
+  });
+});
+router.get("/users", isAuthenticateUser, listUser);
+router.get("/user/:userId", isAuthenticateUser, read);
+router.post("/user-upload", isAuthenticateUser, updateInfo);
+router.post("/user-upload-login", isAuthenticateUser, update);
+router.get("/user", isAuthenticateUser, list);
+router.delete("/user/:userId", isAuthenticateUser, remove);
+
+router.param("userId", isAuthenticateUser, userById);
+
+module.exports = router;
