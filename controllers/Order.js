0217a715a68e1c36c11e6ef9c8467b55852ea9a6
tranvandoan1 <tranlove24@gmail.com>
2022-10-01 18:02:50 +0700

    dấd

--- controllers/Order.js
+++ controllers/Order.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 
 export const create = (req, res) => {
   let order = new Order(req.body);
-  console.log(order);
+
   order.save((err, data) => {
     if (err) {
       res.status(400).json({
@@ -32,7 +32,6 @@ export const read = (req, res) => {
 export const remove = async (req, res) => {
   const { id } = req.body;
   await Order.findByIdAndRemove(id);
-
   Order.find((err, data) => {
     if (err) {
       error: "Không tìm thấy sản phẩm";
@@ -49,33 +48,3 @@ export const list = (req, res) => {
     res.json(data);
   });
 };
-
-export const update = (req, res) => {
-  const order = req.order;
-  order.customer_name = req.body.customer_name;
-  order.id_table = req.body.id_table;
-  order.floor_id = req.body.floor_id;
-  order.sum_price = req.body.sum_price;
-  order.sale = req.body.sale;
-  order.bill = req.body.bill;
-  order.date = req.body.date;
-  order.month = req.body.month;
-  order.year = req.body.year;
-
-  order.save((err, data) => {
-    if (err) {
-      res.status(400).json({
-        error: "Không sửa được sản phẩm",
-      });
-    }
-    res.json(data);
-  });
-};
-// export const readPhoto = (req, res, next) => {
-//     if (req.product.photo.data) {
-//         res.set('Content-Type', req.product.photo.contentType);
-//         return res.send(req.product.photo.data)
-//     }
-//     next()
-
-// }
--- routes/Order.js
+++ routes/Order.js
@@ -8,8 +8,6 @@ router.post("/order", isAuthenticateUser, create);
 router.get("/order", isAuthenticateUser, list);
 router.get("/order/:id", isAuthenticateUser, read);
 
-router.put("/order/:id", isAuthenticateUser, update);
-
 router.post("/order-remove", isAuthenticateUser, remove);
 
 router.param("id", isAuthenticateUser, Id);
